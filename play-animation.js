d01aff2b332d7856b450f6b750269be7be829ad7
RonGissin <45989167+RonGissin@users.noreply.github.com>
2021-01-07 18:23:41 +0200

    added todo

--- play-animation.js
+++ play-animation.js
@@ -4,28 +4,28 @@
         * command: play set animation on configured robot.
         * output: operation success/failure response.
 */
-module.exports = function (RED) {
-  function PlayAnimationNode(config) {
-    RED.nodes.createNode(this, config);
-    var node = this;
+module.exports = function(RED) {
+	function PlayAnimationNode(config) {
+		RED.nodes.createNode(this, config);
+		var node = this;
+		const butter = require('@butter-robotics/mas-javascript-api');
 
-    const butter = require("@butter-robotics/mas-javascript-api");
+		node.on('input', async function(msg) {
+			// create butter client.
+			this.warn('creating butter http client.');
+			const butterHttpClient = new butter.HttpClient(node.robotIp);
 
-    node.on("input", function (msg) {
-      // create butter client.
-      this.debug("creating butter http client.");
-      const butterHttpClient = new butter.HttpClient(node.robotIp);
+			// play animation.
+			// TODO: fix node.animationName is undefined.
+			this.warn(`playing animation - ${node.animationName}`);
+			const resp = await butterHttpClient.playAnimation(node.animationName);
+			this.warn(resp);
+			// send operation result.
+			var respMsg = { payload: 'success' };
+			node.send(respMsg);
+		});
+	}
 
-      // play animation.
-      this.debug(`playing animation - ${node.animationName}`);
-      butterHttpClient.playAnimation(node.animationName);
-
-      // send operation result.
-      var respMsg = { payload: "success" };
-      node.send(respMsg);
-    });
-  }
-
-  // register node type.
-  RED.nodes.registerType("play-animation", PlayAnimationNode);
+	// register node type.
+	RED.nodes.registerType('play-animation', PlayAnimationNode);
 };
