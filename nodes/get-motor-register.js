db2b85c206e1ece0303c1ec33d703909dce06765
ehudb9 <70571624+ehudb9@users.noreply.github.com>
2021-03-02 00:20:10 +0200

    building getMotorRegister

---
+++ nodes/get-motor-register.js
@@ -0,0 +1,57 @@
+/*
+    SetMotorRegister node
+        * input: message (any).
+        * command: set a value to a motor register on configured robot.
+        * output: operation success/failure response.
+*/
+
+module.exports = function(RED) {
+	function GetMotorRegisterNode(config) {
+		RED.nodes.createNode(this, config);
+		this.config = config;
+		var node = this;
+
+		const butterClientProvider = require('../butter-client/butter-client-provider');
+
+		node.on('input', async function(msg) {
+			// create butter client.
+			const butterHttpClient = butterClientProvider.GetClient(node.config.robotIp);
+
+			let robotIp = node.config.robotIp;
+			let motorName = node.config.motorName;
+			let registerName = node.config.registerName;
+			let isDebugMode = node.config.debugMode;
+
+			// check if message has correct json payload - if yes run it instead.
+			if (
+				msg.payload.robotIp != undefined &&
+				msg.payload.motorName != undefined &&
+				msg.payload.registerName != undefined
+			) {
+				robotIp = msg.payload.robotIp;
+				motorName = msg.payload.motorName;
+				registerName = msg.payload.registerName;
+			}
+
+			// play animation.
+			try {
+				if (isDebugMode)
+					this.warn(
+						`getting the value of register ${registerName} of motor ${motorName} of robot ${robotIp} `
+					);
+
+				butter_response = await butterHttpClient.getMotorRegister(motorName, registerName);
+
+				if (isDebugMode) this.warn(`butter response is ${butter_response.data}`);
+				// prints operation result.
+				console.log(butter_response.data);
+				node.send({ payload: butter_response.data });
+			} catch (error) {
+				if (isDebugMode) this.warn(`failed to get register ${registerName}\n${error}`);
+			}
+		});
+	}
+
+	// register node type.
+	RED.nodes.registerType('get-motor-register', GetMotorRegisterNode);
+};
